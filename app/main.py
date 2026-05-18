1a9a6a987cb64222204c43287e3d240ddef1bb7b
jjforsyth15 <jjforsyth15@gmail.com>
2026-05-18 00:35:00 -0700

    updated routing

--- app/main.py
+++ app/main.py
@@ -1,8 +1,12 @@
 from fastapi import FastAPI
 from fastapi.responses import JSONResponse
+from fastapi.middleware.cors import CORSMiddleware
 from app.routes import cards
 from app.routes import auth
 from app.routes import profiles
+from app.routes import users
+from app.routes import analytics
+from app.routes import profile_links
 import os
 from dotenv import load_dotenv 
 
@@ -11,9 +15,23 @@ TEAM_ID = os.getenv("TEAM_ID")
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:5173",
+        "http://127.0.0.1:5173",
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(cards.router)
 app.include_router(auth.router)
 app.include_router(profiles.router)
+app.include_router(analytics.router)
+app.include_router(profile_links.router)
+app.include_router(users.router)
 
 @app.get("/")
 def root():
